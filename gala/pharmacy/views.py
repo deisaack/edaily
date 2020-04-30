4d422ca5d44ee6270e77349167c500f5b5181e29
De Isaac <deisaack@gmail.com>
2020-04-30 21:00:55 +0300

    USSD options

--- gala/pharmacy/views.py
+++ gala/pharmacy/views.py
@@ -49,7 +49,7 @@ def ussd_callback(request):
         resp = "CON Kidney issues\n1. Nutrition/Diet\n2.Food near you\3. Pharmacies"
     elif text == "1*4":
         resp = "CON Heart Issues\n1. Nutrition/Diet\n2.Food near you\3. Pharmacies"
-    elif text in ["1*5*1", "1*4*1", "1*3*1", "1*2*1", "1*1*1", "1*2"]:
+    elif text in ["1*5*1", "1*4*1", "1*3*1", "1*2*1", "1*1*1", "1*5*2", "1*4*2", "1*3*2", "1*2*2", "1*1*2", "1*2"]:
         resp = "END The information will be sent to you shortly"
     else:
         # resp = f"END Invalid option {text} {body}"
