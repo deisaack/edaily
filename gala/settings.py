1181026644b382ecaff466f6db179346dea5dbb4
De Isaac <deisaack@gmail.com>
2020-04-30 13:09:37 +0300

    allowed hosts

--- gala/settings.py
+++ gala/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'fsv$jw9k44vive_-gt0cbfpk+u582h+syy7i2q*v0f4#mlx9+o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
