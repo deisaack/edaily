8431b85ceda69ab582e6a30d8cef74a2a197e8cd
De Isaac <deisaack@gmail.com>
2020-04-30 14:50:13 +0300

    form parser

--- gala/pharmacy/views.py
+++ gala/pharmacy/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.decorators import action, api_view, permission_classes, parser_classes
 from rest_framework import permissions
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser, FormParser
 
 
 @api_view(["post"])
 @permission_classes([permissions.AllowAny])
+@parser_classes([FormParser, JSONParser])
 def ussd_callback(request):
     print("received this data", request.data)
     return Response("CON What would you want to check \n1. My Account \n2. My Balance \n")
