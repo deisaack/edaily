a289a4b83017df4fdc83c837a5fe2e186f02d38d
De Isaac <deisaack@gmail.com>
2020-04-30 20:52:28 +0300

    USSD options

--- gala/pharmacy/views.py
+++ gala/pharmacy/views.py
@@ -39,19 +39,19 @@ def ussd_callback(request):
             continue
     if text == "":
         resp = "CON What would you want to check \n1. Diseases \n2. Covid19"
-    # elif text == "1":
-    #     resp = "Get information on\n1. Diabetes\n2. Hypertesion\n3. Kidney issues\n4.Heart Issues"
-    # elif text == "1*1":
-    #     resp = "CON Diabetes\n1. Nutrition/Diet\n2.Food near you\3. Pharmacies"
-    # elif text == "1*2":
-    #     resp = "CON Hypertesion\n1. Nutrition/Diet\n2.Food near you\3. Pharmacies"
-    # elif text == "1*3":
-    #     resp = "CON Kidney issues\n1. Nutrition/Diet\n2.Food near you\3. Pharmacies"
-    # elif text == "1*4":
-    #     resp = "CON Heart Issues\n1. Nutrition/Diet\n2.Food near you\3. Pharmacies"
-    # elif text == "1*5":
-    #     resp = "END The information will be sent to you shortly"
-    # else:
-    #     resp = f"END Invalid option {text} {body}"
-    resp = f"CON Invalid option {text} {body}"
+    elif text == "1":
+        resp = "Get information on\n1. Diabetes\n2. Hypertesion\n3. Kidney issues\n4.Heart Issues"
+    elif text == "1*1":
+        resp = "CON Diabetes\n1. Nutrition/Diet\n2.Food near you\3. Pharmacies"
+    elif text == "1*2":
+        resp = "CON Hypertesion\n1. Nutrition/Diet\n2.Food near you\3. Pharmacies"
+    elif text == "1*3":
+        resp = "CON Kidney issues\n1. Nutrition/Diet\n2.Food near you\3. Pharmacies"
+    elif text == "1*4":
+        resp = "CON Heart Issues\n1. Nutrition/Diet\n2.Food near you\3. Pharmacies"
+    elif text == "1*5":
+        resp = "END The information will be sent to you shortly"
+    else:
+        # resp = f"END Invalid option {text} {body}"
+        resp = f"CON Invalid option {text} {body}"
     return HttpResponse(resp)
